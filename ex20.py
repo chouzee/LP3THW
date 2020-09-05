e1b27ef0e9ab9bb917d228ef0b7ce9a08cbd29a0
chouzee <anton.ryabchenko.92@gmail.com>
2020-09-05 13:15:28 +0300

    Functions and files

---
+++ ex20.py
@@ -0,0 +1,38 @@
+from sys import argv
+
+script, input_file = argv
+
+# print file content and pass it as 'f'
+def print_all(f):
+    print(f.read())
+
+# got to the start of the file
+def rewind(f):
+    f.seek(0)
+
+# pass line number from var and increment it as much as we have lines
+# then pass file to read lines
+def print_a_line(line_count, f):
+    print(line_count, f.readline(), end=' ')
+
+# here we pass file as an arg and save as object
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
